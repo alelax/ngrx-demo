3bdcae112858a752d53e0536ee66c569b62daa74
alelax <alessandro.lausdei@gmail.com>
2025-01-05 12:17:04 +0100

    complete NGRX basics

--- src/app/core/store/counter/counter.reducer.ts
+++ src/app/core/store/counter/counter.reducer.ts
@@ -1,1 +1,12 @@
-export const counterReducer = () => ({ value: 0 });
+const initialState = { value: 0 };
+export const counterReducer = (state = initialState, action: any) => {
+  switch (action.type) {
+    case 'increment':
+      return { value: state.value + 1 };
+    case 'decrement':
+      return { value: state.value - 1 }
+    case 'reset':
+      return { value: 0}
+  }
+  return state
+};
--- src/app/features/counter/counter.component.ts
+++ src/app/features/counter/counter.component.ts
@@ -10,4 +10,17 @@ import { Store } from '@ngrx/store';
 export default class CounterComponent {
   store = inject(Store);
   counter = this.store.selectSignal(state => state.counter.value);
+
+  inc() {
+    this.store.dispatch({ type: 'increment' });
+  }
+
+  dec() {
+    this.store.dispatch({ type: 'decrement' });
+  }
+
+  reset() {
+    this.store.dispatch({ type: 'reset' });
+  }
+
 }
